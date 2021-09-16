8a409a290de4b7b9b20418aa3ff5f216fb20b64f
Jakelyny <jakelynysousa.a@gmail.com>
2021-09-16 17:37:06 -0300

    .

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+function Calcular() {
+
+    var primeiraNota = parseInt(document.getElementById("primeiraNota").value);
+    var segundaNota = parseInt(document.getElementById("segundaNota").value);
+    
+    var media = (primeiraNota + segundaNota) / 2;
+    var notaFinal = media.toFixed(1);
+    
+    if (notaFinal >= 6) {
+      document.getElementById("resultado").innerHTML = "PARABÉNS, você passou com a média " + notaFinal + " 🥳";
+    } else {
+      document.getElementById("resultado").innerHTML = "OPS, parece que você não passou com a média " + notaFinal + " 😰";
+    }
+  }
